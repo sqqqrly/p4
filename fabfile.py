d857d1c38e105b1b1128a06605f6f9f8a31f0c49
dohlemacher <ohlemacher@ieee.org>
2014-01-02 18:59:09 -0500

    Added fabfile.

---
+++ fabfile.py
@@ -0,0 +1,13 @@
+from fabric.api import lcd, local
+
+def prepare_deployment(trunk):
+    local('manage.py test p4') 
+    local('git add -p && git commit')
+
+def deploy():
+    with lcd('/home/dohlemacher/dev/dj/production/p4/'):
+        local('git pull /home/dohlemacher/dev/dj/dev/p4/')
+        local('python manage.py migrate myapp')
+        local('python manage.py test myapp')
+        local('./manage.py runserver 127.0.0.1:8080')
+
