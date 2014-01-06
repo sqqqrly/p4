1873702f49373a742d14d755e7fde723d4b9228d
dohlemacher <ohlemacher@ieee.org>
2014-01-06 11:17:27 -0500

    Added choice

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from polls.models import Poll
+from polls.models import Choice, Poll
 
-admin.site.register(Poll)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+            (None, {'fields': ['question']}),
+            ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Poll, PollAdmin)
 
