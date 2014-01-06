d2d3f185761374d4243d3111dbd23f2f1f504665
dohlemacher <ohlemacher@ieee.org>
2014-01-06 11:25:08 -0500

    Fixed polls to support was_published_recently

--- polls/models.py
+++ polls/models.py
@@ -5,6 +5,8 @@ class Poll(models.Model):
     pub_date = models.DateTimeField('date published')
     def __unicode__(self):
         return self.question
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
