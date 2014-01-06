437e32e38edac10d1ebf85cc0268e20e103f7aeb
dohlemacher <ohlemacher@ieee.org>
2014-01-06 14:51:52 -0500

    Switch to mysql from sqllite.

--- p4/settings.py
+++ p4/settings.py
@@ -59,9 +59,15 @@ WSGI_APPLICATION = 'p4.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/home/dohlemacher/dev/dj/dev/p4/db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': '/home/dohlemacher/dev/dj/dev/p4/db.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'p4db',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'dohlemacher',
+        'PASSWORD': 'hdsops',
     }
 }
 
