6b4ebe9150900e947b949ec15b4e13f5841d765e
dohlemacher <ohlemacher@ieee.org>
2014-01-07 13:14:03 -0500

    Implemented more view

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
-from polls.models import Poll
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from polls.models import Choice, Poll
 
 def index(request):
     # Load template, fill context, return HttpResponse
@@ -17,9 +16,23 @@ def detail(request, poll_id):
 
 def results(request, poll_id):
     poll = get_object_or_404(Poll, pk=poll_id)
-    return HttpResponse("You're looking for the results of poll %s." % poll_id)
+    return render(request, 'polls/results.html', {'poll':poll})
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = poll.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        # Redesplay the poll voting from
+        return render(request, 'polls/detail.html', {
+            'poll': poll,
+            'error_message': "You did not select a choice.",
+            })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully handling a POST.
+        # Prevents double posts if user hits the back button.
+        return HttpResponseRedirect(reverse('p4:results', args=(poll.id,)))
 
 
